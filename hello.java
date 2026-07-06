be5521158d10a224f6bb71be9321f845ddb4e9f5
Manogna K V <manumanogna60@gmail.com>
2026-07-06 11:52:06 +0530

    this is form the jimmy

--- hello.java
+++ hello.java
@@ -1,6 +1,16 @@
 public class hello{
     
     public static void main(String[] args){
+<<<<<<< Updated upstream
         System.out.println("hello world naliece");
+=======
+        System.out.println("");
+        int a = 90;
+        int b = 89;
+        int c = a+b;
+        int l = 90;int mnm = 89;
+
+        int mnma = 89;
+>>>>>>> Stashed changes
     }
 }
